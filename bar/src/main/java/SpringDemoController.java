4b9c30213b6ff5ad5b86528b2e74dfc243535ce4
Jingwen Owen Ou <jingweno@gmail.com>
2011-02-23 11:04:07 -0800

    Added foo & bar examples.

---
+++ bar/src/main/java/SpringDemoController.java
@@ -0,0 +1,20 @@
+package mavenjettydemo.foo;
+
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class SpringDemoController implements Controller
+{
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        System.out.println("***** Returning \"hello.jsp\" with SpringDemoController *****");
+        return new ModelAndView("hello.jsp");
+    }
+}
---
+++ foo/src/main/webapp/hello.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>Spring Demo</title>
+    <link rel="stylesheet" href="styles/spring-demo.css" type="text/css">
+  </head>
+  <body>
+  
+    <h1>Spring Demo</h1>
+    <br/>
+    
+  	Hello World from foo!
+    
+  </body>
+</html>
